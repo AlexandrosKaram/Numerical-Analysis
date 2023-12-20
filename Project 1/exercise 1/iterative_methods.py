0f6e89deef92203201221f893555aeadb7335d1f
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-20 17:14:28 +0200

    Exercise 1 - improve comments.

--- Project 1/exercise 1/iterative_methods.py
+++ Project 1/exercise 1/iterative_methods.py
@@ -1,13 +1,15 @@
-# recursive bisection function
+# bisection function to find an approach of the root of f in the interval [a, b]
 def bisection(f, a, b):
-    m = (a+b)/2
+    m = (a+b)/2   # mid-point
 
-    # final state
+    # termination condition
     if f(m) == 0:
-        return round(m, 5)
+        return round(m, 5)   
     
     # recursive state
     if (f(m)*f(a)<0):
+        # recur on the left half of the interval
         return bisection(f, a, m)
     else:
+        # recur on the right half of the interval
         return bisection(f, m, b)
