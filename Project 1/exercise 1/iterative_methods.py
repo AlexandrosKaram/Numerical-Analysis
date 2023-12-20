ef9a92ce5e7ed40722c06bb30ff8cbade31ba71f
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-20 18:14:58 +0200

    Exercise 1 - create Newton-Raphson function.

--- Project 1/exercise 1/iterative_methods.py
+++ Project 1/exercise 1/iterative_methods.py
@@ -1,15 +1,28 @@
-# bisection function to find an approach of the root of f in the interval [a, b]
+import sympy as sp
+
+# approach of the root of function f in the interval (a,b) using the Bisection method
 def bisection(f, a, b):
     m = (a+b)/2   # mid-point
 
     # termination condition
-    if f(m) == 0:
+    if abs(f(m)) < 10**(-5):   # 5 decimal digit precision
         return round(m, 5)   
     
-    # recursive state
+    # recursive step
     if (f(m)*f(a)<0):
         # recur on the left half of the interval
         return bisection(f, a, m)
     else:
         # recur on the right half of the interval
         return bisection(f, m, b)
+
+
+# approach of the root of function f using the Newton-Raphson method with initial guess - x
+def newton_raphson(f, df_dx, x):
+    # termination condition
+    if abs(f(x)) < 10**(-5):  
+        return round(x, 5)
+    
+    x_next = x - f(x)/df_dx(x)
+    # recursive step
+    return newton_raphson(f, df_dx, x_next)
--- Project 1/exercise 1/main.py
+++ Project 1/exercise 1/main.py
@@ -1,6 +1,6 @@
 import function_graph as cf
 import numpy as np
-import iterative_methods as im
+import iterative_methods as it
 
 
 # define our function
@@ -8,14 +8,22 @@ def f(x):
     return 14*x*np.exp(x-2) - 12*np.exp(x-2) - 7*x**3 + 20*x**2 - 26*x + 12
     
 
+# derivative of our f function in respect of x
+def df_dx(x):
+    return -26 + 40*x - 21*x**2 + 2*np.exp(x-2)*(1+7*x)
+
+
 # main
 def main():
     # print the graph of our function
     cf.print_graph(f, (0, 3), 1000)
 
     # calculate function root using iterative methods
-    print("Root of f using bisection method:", im.bisection(f, 0, 3))
+    print("Root of f using:")
+    print("a. Bisection method:", it.bisection(f, 0, 3))
+    print("b. Newton-Raphson method:", it.newton_raphson(f, df_dx, 0))
 
 # call main
 if __name__ == "__main__":
     main()
+    
