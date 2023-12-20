3630a0f666fd66d9ba246dad9c7a54614fa26250
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-20 16:20:52 +0200

    Exercise 1 - Organize code into functions.

--- Project 1/exercise 1/main.py
+++ Project 1/exercise 1/main.py
@@ -7,11 +7,27 @@ def f(x):
     return 14*x*np.exp(x-2) - 12*np.exp(x-2) - 7*x**3 + 20*x**2 - 26*x + 12
     
     
-# generate x and y values
-x_values = np.linspace(0, 3, num=1000)
-y_values = f(x_values)
+# function to print graph
+def print_graph(function, x_range, num=1000):
+    # generate x and y values
+    x_values = np.linspace(x_range[0], x_range[1], num)
+    y_values = function(x_values)
 
-# plot the function
-plt.figure()
-plt.plot(x_values, y_values)
-plt.show()
+    # plot the function
+    plt.figure()
+    plt.plot(x_values, y_values)
+    plt.title("f Graph")
+    plt.xlabel('x-ayis')
+    plt.ylabel('y-axis')
+
+    plt.show()
+
+
+# main
+def main():
+    print_graph(f, (0, 3), 1000)
+
+
+# call main
+if __name__ == "__main__":
+    main()
