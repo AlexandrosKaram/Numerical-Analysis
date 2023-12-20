3601e77c491dc34d0c53e8023040bb23f6365f84
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-20 15:57:21 +0200

    Exercise 1 - plot function.

--- Project 1/exercise 1/main.py
+++ Project 1/exercise 1/main.py
@@ -1,11 +1,17 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import math
+import numpy as np
 
-# define the function
+
+# define function
 def f(x):
-    if 0<=x<=3:
-        return 14*x*math.exp(x-2) - 12*math.exp(x-2) - 7*x**3 + 20*x**2 - 26*x + 12
-    else:
-        return None
+    return 14*x*np.exp(x-2) - 12*np.exp(x-2) - 7*x**3 + 20*x**2 - 26*x + 12
     
+    
+# generate x and y values
+x_values = np.linspace(0, 3, num=1000)
+y_values = f(x_values)
+
+# plot the function
+plt.figure()
+plt.plot(x_values, y_values)
+plt.show()
