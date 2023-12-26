e5ecfc0dd8faeb060edd1a3336705286e1fef674
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-26 12:09:43 +0200

    Exercise 4 - 4a done.

---
+++ Project 1/exercise4.py
@@ -0,0 +1,43 @@
+from linear_equation_methods import print_matrix, round_matrix, initialize_matrix
+
+q = 0.15   # Probability of moving to a page
+n = 15   # Total pages
+
+
+# Main
+def main():
+    # Αdjacency matrix
+    A = [
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
+    ]
+
+    # Exercise 4a
+    for j in range(n):
+        nj = sum(A[j][i] for i in range(n))   # Sum of connections to j
+        s = 0
+        for i in range(n):
+            s += q/n + A[j][i]*(1-q)/nj
+        if s != 1:   # Sum of j-column not equal to 1
+            print("Google matrix is not stochastic.")
+            break
+    else:   # Loop ended 
+        print("Google matrix indeed is stochastic.")
+
+
+# Call main
+if __name__ == "__main__":
+    main()
