00192a42702d0ef7c02f751a031854df31894bf1
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-26 12:10:39 +0200

    Exercise 4 - Create google matrix.

--- Project 1/exercise4.py
+++ Project 1/exercise4.py
@@ -4,6 +4,17 @@ q = 0.15   # Probability of moving to a page
 n = 15   # Total pages
 
 
+def create_google_matrix(A):
+    """Function that receives Adjacency matrix and creates Google matrix."""
+    G = initialize_matrix(15)   # Initialize 15x15 Google matrix with zeros
+
+    for j in range(n):
+        nj = sum(A[j][i] for i in range(n))
+        for i in range(n):
+            G[i][j] = q/n + A[j][i]*(1-q)/nj
+    
+    return G
+
 # Main
 def main():
     # Αdjacency matrix
@@ -36,6 +47,9 @@ def main():
             break
     else:   # Loop ended 
         print("Google matrix indeed is stochastic.")
+
+    # Exercise 4b
+    G = create_google_matrix(A)
 
 
 # Call main
