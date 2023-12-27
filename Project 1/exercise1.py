b5900f534895eeb59d1de91217b8a35ddb31b115
Alexandros Karamouchtaris <alexkaramuxtaris@gmail.com>
2023-12-28 01:27:33 +0200

    Exercise 1 - format code.

--- Project 1/exercise1.py
+++ Project 1/exercise1.py
@@ -5,14 +5,7 @@ import iterative_methods as it
 
 # Define function f
 def f(x):
-    return (
-        14 * x * np.exp(x - 2)
-        - 12 * np.exp(x - 2)
-        - 7 * x**3
-        + 20 * x**2
-        - 26 * x
-        + 12
-    )
+    return 14 * x * np.exp(x - 2) - 12 * np.exp(x - 2) - 7 * x**3 + 20 * x**2 - 26 * x + 12
 
 
 # Derivative of function f in respect of x
@@ -25,37 +18,34 @@ def main():
     # Print the graph of our function
     cf.display_graph(f, (0, 3), 1000)
 
+    print("Check for quadratic convergence for both roots.")
     # Calculate roots of the function f using iterative methods
-    bisection_results = (it.bisection(f, 0.5, 1), it.bisection(f, 1.5, 2))
+    bisection_results = (
+        it.bisection(f, 0.5, 1), 
+        it.bisection(f, 1.5, 2.1)
+    )
     newton_raphson_results = (
         it.newton_raphson(f, df_dx, 0.5, print_convergence=True),
         it.newton_raphson(f, df_dx, 1.5, print_convergence=True),
     )
-    secant_method_results = (it.secant(f, 0.5, 1), it.secant(f, 1.5, 2.5))
+    secant_method_results = (
+        it.secant(f, 0.5, 1), 
+        it.secant(f, 1.5, 2.1)
+    )
+    
+    print("We can clearly see that f converges quadratically only for the first root.\n")
 
     # Print the results
     print("Approximation of roots using:")
     print("1. Bisection method:")
-    print(
-        f"\ta. Root: {bisection_results[0][0]} Iterations needed: {bisection_results[0][1]}"
-    )
-    print(
-        f"\tb. Root: {bisection_results[1][0]} Iterations needed: {bisection_results[1][1]}"
-    )
+    print(f"\ta. Root: {bisection_results[0][0]} Iterations needed: {bisection_results[0][1]}")
+    print(f"\tb. Root: {bisection_results[1][0]} Iterations needed: {bisection_results[1][1]}")
     print("2. Newton-Raphson method:")
-    print(
-        f"\ta. Root: {newton_raphson_results[0][0]} Iterations needed: {newton_raphson_results[0][1]}"
-    )
-    print(
-        f"\tb. Root: {newton_raphson_results[1][0]} Iterations needed: {newton_raphson_results[1][1]}"
-    )
+    print(f"\ta. Root: {newton_raphson_results[0][0]} Iterations needed: {newton_raphson_results[0][1]}")
+    print(f"\tb. Root: {newton_raphson_results[1][0]} Iterations needed: {newton_raphson_results[1][1]}")
     print("3. Secant method:")
-    print(
-        f"\ta. Root: {secant_method_results[0][0]} Iterations needed: {secant_method_results[0][1]}"
-    )
-    print(
-        f"\tb. Root: {secant_method_results[1][0]} Iterations needed: {secant_method_results[1][1]}"
-    )
+    print(f"\ta. Root: {secant_method_results[0][0]} Iterations needed: {secant_method_results[0][1]}")
+    print(f"\tb. Root: {secant_method_results[1][0]} Iterations needed: {secant_method_results[1][1]}")
 
 
 # Call main
--- Project 1/tempCodeRunnerFile.py
+++ Project 1/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-)
+    bisection_results = (it.bisection(f, 0, 1.5), it.bisection(f, 1.5, 3))
+    newton_raphson_results = (
+        it.newton_raphson(f, df_dx, 0, print_convergence=True),
+        it.newton_raphson(f, df_dx, 3, print_convergence=True),
+    )
+    secant_method_results = (it.secant(f, 0, 1.5), it.secant(f, 1.5, 3))
